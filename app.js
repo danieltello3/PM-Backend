1f2b73938cc96534d49c41fa370992859fd87a49
Brillitt <brillitt612@gmail.com>
2023-06-19 21:32:54 -0500

    commit

--- app.js
+++ app.js
@@ -167,10 +167,11 @@ app.post('/user/create_account', async (req, res, next) => {
   var usuario = req.body.user
   var contrasenia = req.body.password;
   var correo = req.body.email;
+  var nombre = req.body.username
   var image_url = 'user_default.png';
   // logic
   var query1 = `SELECT COUNT(*) AS count FROM users WHERE user=? OR email=?`;
-  var query2 = `INSERT INTO users (user, password, email, image_url) VALUES (?, ?, ?, ?)`;
+  var query2 = `INSERT INTO users (user, password, name, email, image_url) VALUES (?, ?, ?, ?, ?)`;
   let connection = dbApp()
   connection.get(query1, [usuario, correo], (err, row) => {
     if (err) {
@@ -178,7 +179,7 @@ app.post('/user/create_account', async (req, res, next) => {
       res.status(500).send('Error: Ocurrio un error');
     }
     if (row['count'] == 0){
-      connection.run(query2, [usuario, contrasenia, correo, image_url], function(err) {
+      connection.run(query2, [usuario, contrasenia, nombre, correo, image_url], function(err) {
         if (err) {
           connection.close();
           res.status(500).send('Error al crear al nuevo usuario')
@@ -190,6 +191,7 @@ app.post('/user/create_account', async (req, res, next) => {
           email: row['email'], 
           image_url: row['image_url']
         }
+        console.log(response)
         res.status(200).send(response)
       });
     }else{
@@ -453,19 +455,20 @@ app.post('/user/validate/password', async (req, res, next) => {
   // data
   var id = req.body.id;
   var password = req.body.password;
+  console.log(id,password)
   // logic
   let connection = dbApp()
   let sql = `SELECT COUNT(*) AS count FROM users WHERE id=? AND password=?`;
   connection.get(sql, [id, password], (err, row) => {
     if (err) {
       console.error(err);
-      res.status(500).send({status: "Error", message: 'ups, ocurrió un error'});
+      res.status(500).send({status: "Error", message: 'Error: Ocurrió un error'});
     }
     connection.close();
     if (row['count'] == 1){
       res.status(200).send({status: "OK", message: 'Contraseña valida'})
     }else{
-      res.status(200).send({status: "Error", message: 'Contraseña antigua incorrecta'})
+      res.status(200).send({status: "Error", message: 'Error: Contraseña antigua incorrecta'})
     }
   });
 });
@@ -489,8 +492,8 @@ app.get('/user/get_username',async(req,res)=> {
  
 })
 
-app.get('user/get_email',async(req,res) => {
-  let correo = req.query.correo;
+app.get('/user/get_email',async(req,res) => {
+  let correo = req.query.email;
   let connection = dbApp()
   let sql = `SELECT id FROM users WHERE email=?`;
   connection.get(sql, [correo], (err, row) => {
@@ -499,11 +502,7 @@ app.get('user/get_email',async(req,res) => {
       res.status(500).send('ocurrió un error');
     }
     var response = {
-      id:row['id'],
-      user: row['user'], 
-      name: row['name'], 
-      email: row['email'], 
-      image_url: row['image_url']
+      id:row['id']
     }
     res.status(200).send(response)
     connection.close();
