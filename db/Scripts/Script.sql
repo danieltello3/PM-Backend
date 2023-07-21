1f8ff318e3689a8962f9b784c6d515eb4be41ffa
Brillitt <brillitt612@gmail.com>
2023-07-21 12:14:07 -0500

    cambios en fetch one

--- db/Scripts/Script.sql
+++ db/Scripts/Script.sql
@@ -7,5 +7,23 @@ delete from users
 where id in ('20')
 
 UPDATE users
-SET name = 'Super Administrador', password = '123'
+SET name = 'Super Administrador', password = '123456'
 WHERE id = 1;
+
+ALTER TABLE users
+ADD COLUMN id_aux int null;
+
+ALTER TABLE users
+ADD COLUMN uid varchar(30) null;
+
+UPDATE users 
+SET id_aux = id;
+
+ALTER TABLE users ALTER COLUMN id TYPE VARCHAR(30);
+
+UPDATE users
+SET uid = 'HM3yepUueHWDNRJqtgABLpvxSvQ2'
+WHERE id = 1;
+
+
+
